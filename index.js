339fed33c81e6edfc0253012e9f3c83bb6cba2e2
Interview Master <dev@interviewmaster.org>
2025-08-14 13:22:31 +0100

    Initialised

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Export app for testing without starting the server
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
+
